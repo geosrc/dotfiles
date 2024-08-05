67e69fc9e32b1c6c97ec8aa9a1804675eab7f918
Fabian Pastor <fabian_pastor@trimble.com>
2024-08-05 09:42:34 +0200

    Update pwsh profile to use Starship

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -7,39 +7,32 @@
 #    . $p
 # }
 
-Set-Alias -Name l -Value Get-ChildItem
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 
 # Make interactive consoles fancy
 if ($host.Name -eq 'ConsoleHost') {
 
+    Set-Alias -Name l -Value Get-ChildItem
+
     # Show icons in Get-ChildItem
     # > Install-Module -Name Terminal-Icons -Repository PSGallery
-    if (Get-Module -ListAvailable -Name Terminal-Icons) {
-        Import-Module -Name Terminal-Icons
-    }
-    # else {
-    #     Write-Host -ForegroundColor Red "`u{f00d}"
-    # }
+    Import-Module -Name Terminal-Icons -ErrorAction SilentlyContinue
 
     # Predictive promt
     # > Install-Module PSReadLine
-    if (Get-Module -ListAvailable -Name PSReadLine) {
-        Import-Module PSReadLine
-        Set-PSReadLineOption -PredictionSource History
-        Set-PSReadLineOption -PredictionViewStyle ListView
-        Set-PSReadLineOption -EditMode Windows
-    }
+    Import-Module PSReadLine
+    
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-    # Load oh-my-posh with custom theme
-    # Install from https://ohmyposh.dev:
-    # > winget install JanDeDobbeleer.OhMyPosh -s winget
-    # or
-    # > Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
-    # Requires https://www.nerdfonts.com/ and the new AtlasEngine in Terminal.app to work
-    if (Get-Command "oh-my-posh" -ErrorAction SilentlyContinue) 
-    {
-        oh-my-posh init pwsh --config "$HOME/dotfiles/fabian.omp.json" | Invoke-Expression
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+    Set-PSReadLineOption -EditMode Windows
+
+    if (Get-Command "starship" -ErrorAction SilentlyContinue) {
+        $ENV:STARSHIP_CONFIG = "$HOME/dotfiles/starship.toml"
+        Invoke-Expression (&starship init powershell)
     }
 }
 
